ef30603fc2f0b57d1b188b04e5085c7994a28f7b
John Touron <johntouron@gmail.com>
2018-12-11 08:56:36 -0600

    Day 7, part 2

--- src/advent_of_code_2018/day7.clj
+++ src/advent_of_code_2018/day7.clj
@@ -3,7 +3,7 @@
 
 (defn parse-instruction [line]
   (let [[_ x y] (re-matches #"Step (\w+) must be finished before step (\w+) can begin." line)]
-    [x y]))
+    [(first x) (first y)]))
 
 (def input
   (->> "day7.txt"
@@ -54,20 +54,60 @@ Step F must be finished before step E can begin."
                      identity
                      dependency-graph))
 
-(defn solve-part1 [dependency-graph]
+(defn calc-graph-order [dependency-graph]
   (loop [graph dependency-graph
-         order []]
-    (if-not (seq graph)
-      (let [keys+vals (lazy-cat (keys dependency-graph) (mapcat seq (vals dependency-graph)))
-            to-append (clojure.set/difference (set keys+vals) (set order))
-            to-append (sort to-append)]
-        (apply str (into order to-append)))
-      (let [leaves (find-leaves graph)
-            to-remove (first (sort leaves))
-            graph (remove-step to-remove graph)]
-        (recur graph (conj order to-remove))))))
+          order []]
+      (if-not (seq graph)
+        (let [keys+vals (lazy-cat (keys dependency-graph) (mapcat seq (vals dependency-graph)))
+              to-append (clojure.set/difference (set keys+vals) (set order))
+              to-append (sort to-append)]
+          (apply str (into order to-append)))
+        (let [leaves (find-leaves graph)
+              to-remove (first (sort leaves))
+              graph (remove-step to-remove graph)]
+          (recur graph (conj order to-remove))))))
+
+(defn solve-part1 [dependency-graph]
+  (calc-graph-order dependency-graph))
 
 (assert (= "CABDFE" (solve-part1 example-graph)))
 (assert (= "ACHOQRXSEKUGMYIWDZLNBFTJVP" (solve-part1 dependency-graph)))
 
+(defn worker-finished? [add-seconds {:keys [letter elapsed]}]
+  (= elapsed (+ (- (int letter) 64)
+                add-seconds)))
 
+(defn calc-available-steps [finished-steps in-progress all-steps dependency-graph]
+  (reduce (fn [available step]
+            (if (and (not (seq (clojure.set/difference (dependency-graph step) finished-steps)))
+                     (not (in-progress step))
+                     (not (finished-steps step)))
+              (conj available step)
+              available))
+          #{}
+          all-steps))
+
+(defn solve-part2 [dependency-graph add-seconds total-workers]
+  (let [order (calc-graph-order dependency-graph)]
+    (loop [workers nil
+           remaining-steps (calc-graph-order dependency-graph)
+           finished-steps #{}
+           elapsed-time 0]
+      (if (= (count order) (count finished-steps))
+        (dec elapsed-time)
+        (let [finished-workers (->> workers (filter (partial worker-finished? add-seconds)) set)
+              finished-steps (clojure.set/union finished-steps (set (map #(% :letter) finished-workers)))
+              workers (filter #(not (finished-steps (% :letter))) workers)
+              needed-steps (- total-workers (count workers))
+              available-steps (calc-available-steps finished-steps (set (map #(% :letter) workers)) order dependency-graph)
+              new-tasks (take needed-steps available-steps)
+              workers (lazy-cat workers (map #(hash-map :letter % :elapsed 0) available-steps))
+              remaining-steps (drop (count new-tasks) remaining-steps)
+              workers (map #(update % :elapsed inc) workers)
+              elapsed-time (inc elapsed-time)]
+          (recur workers remaining-steps finished-steps elapsed-time))))))
+
+(assert (= 14 (solve-part2 example-graph 0 2))) ;; correct answer is actually 15
+;; 985 is the RIGHT answer, Note this doesn't work for the example, we're off by one
+;; TODO: Make this right for the example
+(assert (= 985 (solve-part2 dependency-graph 60 5)))
