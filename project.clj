db19bdc448bf91133d2ce710865b068a3c5299c7
John Touron <johntouron@gmail.com>
2019-01-08 07:33:40 -0600

    Removed cider-nrepl from dependencies

--- project.clj
+++ project.clj
@@ -4,5 +4,4 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/core.match "0.3.0-alpha5"]]
-  :profiles {:dev {:plugins [[cider/cider-nrepl "0.18.0"]]}})
+                 [org.clojure/core.match "0.3.0-alpha5"]])
